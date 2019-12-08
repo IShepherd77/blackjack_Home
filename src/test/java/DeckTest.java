f8420faac51f5b151364fb35c535df21d3e26bae
IShepherd77 <ianshepherd77@gmail.com>
2019-12-08 18:01:03 +0000

    added Deck Test

---
+++ src/test/java/DeckTest.java
@@ -0,0 +1,33 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class DeckTest{
+
+    private Deck deck;
+
+    @Before
+    public void setUp(){
+        deck = new Deck();
+    }
+
+    @Test
+    public void deckHas52Cards(){
+        assertEquals(52, deck.getNumOfCards());
+    }
+
+    @Test
+    public void deckIsShuffled(){
+        Deck deck2 = new Deck();
+        assertFalse(deck.equals(deck2));
+    }
+
+    @Test
+    public void canTakeCard(){
+        Card card = deck.takeCard();
+        assertNotNull(card);
+    }
+}
