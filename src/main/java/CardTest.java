f277a19e23a0800af74b6cb94a51bb86837b35e3
IShepherd77 <ianshepherd77@gmail.com>
2019-12-08 14:09:57 +0000

    added card test

---
+++ src/main/java/CardTest.java
@@ -0,0 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CardTest{
+
+    private Card card;
+
+
+@Before
+    public void setUp(){
+    card = new Card(Suit.SPADES, Rank.ACE);
+}
+
+@Test
+    public void cardHasSuit(){
+    assertEquals(Suit.SPADES, card.getSuit());
+}
+
+@Test
+    public void cardHasRank(){
+    assertEquals(Rank.ACE, card.getRank());
+}
+
+@Test
+    public void cardHasValue(){
+    assertEquals(1, card.getValue());
+}
+
+}
