2a322dabda01315148e66dc3aa8b89c35047ef5d
IShepherd77 <ianshepherd77@gmail.com>
2020-01-08 16:01:45 +0000

    added triel coffe class

---
+++ src/main/java/Coffee.java
@@ -0,0 +1,11 @@
+ class Coffee {
+
+     public static void main(String[] args) {
+         int cupsOfCoffee = 1;
+
+         while (cupsOfCoffee <=100){
+             System.out.println("Ian drinks cup of coffee number " + cupsOfCoffee);
+             cupsOfCoffee++;
+         }
+     }
+}
